2a8fd1269f4b1f41e366c7b230c1cbb0005fc826
ocampodarem <47321740+ocampodarem@users.noreply.github.com>
2019-02-11 12:34:10 -0300

    clase 3

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -2,5 +2,27 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
+
+edad = document.getElementById("edad").value;
+
+if(edad>=17){
+alert("la persona es mayor de edad")
+}
+else{
+
+alert("la persona es menor de edad")
+}
+
+
+}
+
+
+
+
+
+
+
+
 
 }//FIN DE LA FUNCIÓN
